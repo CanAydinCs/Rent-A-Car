050c5fdd82e5a381699718234da6620b0d442452
Mehmet Can Aydın <mehmet1can1aydin@gmail.com>
2023-12-02 18:00:59 +0300

    Automating Ui

--- src/main/java/kodlama/io/rentACar/userInterfaces/VaadinUi.java
+++ src/main/java/kodlama/io/rentACar/userInterfaces/VaadinUi.java
@@ -7,21 +7,52 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 @Route("")
 public class VaadinUi extends VerticalLayout {
+    private VerticalLayout baseLine;
 
-    private TextField inputField = new TextField("Enter text");
-    private TextField outputField = new TextField();
+    private VerticalLayout layoutToRemove;
 
     public VaadinUi() {
+        baseLine = BaseLineCreator();
+        
+        add(baseLine);
+    }
+
+    private VerticalLayout BaseLineCreator(){
+        Button btnTestShower = new Button("Show Test Ui", event -> {
+            TestUi();
+            
+        });
+
+        VerticalLayout layout = new VerticalLayout(btnTestShower);
+        layout.setSizeFull();
+        layout.setJustifyContentMode(JustifyContentMode.CENTER);
+        layout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+
+        return layout;
+    }
+
+    private void TestUi(){
+        if(layoutToRemove != null)
+        {
+            remove(layoutToRemove);
+        }
+
+        TextField inputField = new TextField("Enter Text here:");
+        TextField outputField = new TextField();
+
         Button button = new Button("Submit", event -> {
-            String input = inputField.getValue();
-            outputField.setValue("You entered: " + input);
+            String input =  "You entered : " + inputField.getValue();
+            outputField.setValue(input);
         });
 
         outputField.setReadOnly(true);
+
         VerticalLayout layout = new VerticalLayout(inputField, button, outputField);
         layout.setSizeFull();
         layout.setJustifyContentMode(JustifyContentMode.CENTER);
         layout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         add(layout);
+
+        layoutToRemove = layout;
     }
 }
